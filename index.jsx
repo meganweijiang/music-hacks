4e3f8566f114873d081aa7f72ff069092b2cee7f
Megan Weijiang <meganweijiang@utexas.edu>
2017-04-15 12:43:21 -0500

    buttons to play sounds

--- index.jsx
+++ index.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import Cat from 'cat'
+import Play from 'play'
 
 class App extends Component {
 	render() {
-		let someName = "meowgan"
 		return(
 			<div>
-				<Cat name={someName}/>
-				<p>Hello, world!</p>
-				<Cat/>
+				<Play url="audio/1.mp3" name="1" />
+				<Play url="audio/2.mp3" name="2" />
+				<Play url="audio/3.mp3" name="3" />
+				<Play url="audio/4.mp3" name="4" />
+				<Play url="audio/5.mp3" name="5" />
+				<Play url="audio/6.mp3" name="6" />
+				<Play url="audio/7.mp3" name="7" />
+				<Play url="audio/8.mp3" name="8" />
 			</div>
 		)
 	}
---
+++ play.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Sound from 'react-sound'
+ 
+export default class Play extends Component {
+  constructor(props) {
+	  super(props)
+	  this.state = {
+	  	playStatus: Sound.status.STOPPED
+	  }
+	  this.playSong = this.playSong.bind(this)
+  }
+
+  playSong() {
+  	this.setState({playStatus: Sound.status.PLAYING})
+  }
+  render() {
+    return (
+	  <div>
+      <Sound 
+        url={this.props.url}
+        playStatus={this.state.playStatus} />
+      	<button onClick={this.playSong}>{this.props.name}</button>
+    </div>
+    )
+  }
+}
